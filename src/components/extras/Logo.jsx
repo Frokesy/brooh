9143d820f3f9463c321dfee660e0a2ee70306b46
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-27 04:23:28 +0100

    navbar designed

--- src/components/extras/Logo.jsx
+++ src/components/extras/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className="text-[40px] font-bold">
+    <div className="lg:text-[40px] text-[32px] font-bold">
       <span className="uppercase text-red-500">b</span>
       <span className="text-amber-500">r</span>
       <span className="text-green-500">o</span>
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,85 @@
 import React from 'react'
 import Logo from '../extras/Logo'
+import { FaGithub, FaInstagram, FaTwitter, FaBars } from 'react-icons/fa'
+import {
+    Drawer,
+    DrawerBody,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+  } from '@chakra-ui/react'
+
 
 const Header = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
   return (
-    <div className="bg-[url('/public/assets/header.jpg')] text-white bg-center h-screen bg-cover bg-no-repeat w-screen">
-      <div className="absolute bg-[#000] opacity-75 h-screen w-screen">
-            <div className="w-[80vw] mx-auto flex justify-between mt-[5vh]">
+    <div className="bg-[url('/public/assets/header.jpg')] text-white bg-center h-[98vh] bg-cover bg-no-repeat w-screen">
+      <div className="absolute bg-[#000] opacity-75 h-[98vh] w-screen">
+            <div className="lg:w-[80vw] w-[90vw] mx-auto flex justify-between items-center mt-[2vh]">
                <Logo /> 
-               <h2>Hello world</h2> 
-               <h2>Hello world</h2> 
+                <div className="lg:flex hidden justify-between text-[14px] font-[Montserrat] items-center space-x-14">
+                    <span>Home</span>
+                    <span>Tech</span>
+                    <span>Politics</span>
+                    <span>Entertainment</span>
+                    <span>Life</span>
+                    <span>NFTs</span>
+
+                </div> 
+                <div className="lg:flex hidden space-x-6 text-[14px] items-center">
+                    <FaTwitter />
+                    <FaInstagram />
+                    <FaGithub />
+                </div>
+
+             {/**Mobile Navbar */}   
+          <div className="lg:hidden md:hidden items-center">
+            <span ref={btnRef} onClick={onOpen} className="text-[18px] text-amber-500">
+                <FaBars />
+            </span>
+        <Drawer
+          isOpen={isOpen}
+          placement='bottom'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay
+          px={4}
+          size='md'
+          />
+          <DrawerContent
+          bg='#1a0f00'
+          >
+            <div className="text-red-500 mt-4 mb-10 flex justify-end w-[95vw] text-[13px]"><DrawerCloseButton /></div>
+            <DrawerBody
+            >
+            <div className="w-[90vw] mx-auto gap-x-10 gap-y-6 font-mono text-gray-300 text-[15px] grid grid-cols-3 mb-10">
+                    <span>
+                        Home
+                    </span>
+                    <span>
+                        Tech
+                    </span>
+                    <span>
+                        Politics
+                    </span>
+                    <span>
+                        NFTs
+                    </span>
+                    <span>
+                        Life
+                    </span>
+                    <span>
+                        Entertainment
+                    </span>
+            </div>
+            </DrawerBody>
+          </DrawerContent>
+            
+        </Drawer>
+        </div>
             </div>
       </div>
     </div>
